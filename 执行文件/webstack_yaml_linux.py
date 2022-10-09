def301777fa84aa9d01462163165f363d1678281
yang_potato <yangtudou1220@icloud.com>
2022-10-10 01:02:38 +0800

    gx

--- 执行文件/webstack_yaml_linux.py
+++ 执行文件/webstack_yaml_linux.py
@@ -4,66 +4,99 @@
 import json
 from ruamel.yaml import YAML
 yaml = YAML()
-from WebSiteInfo import *
 
-with open('./执行文件/TaxonomyInfo.json','r',encoding='utf8')as fp:
-    TaxonomyInfo = json.load(fp)
+# 读取网站数据库
+with open('./执行文件/WebSiteInfo.json','r',encoding='utf8')as fp:
+    WebSiteInfo = json.load(fp)
 
+TaxonomyInfoWithoutTerms = []
+# TaxonomyInfoWithTerms = []
 
-# 常用
-TaxonomyInfo[0]["links"] = [
-    土豆仓库,
-    Github,
-    DockerHub,
-    Pinterest,
-    Behance,
-    Dribbble,
-    Medium,
-    Youtube
+# 没有子分类的数据
+TaxonomyInfoWithoutTerms_infos = [
+    [
+        "常用",
+        "fa-star",
+        [
+            "土豆仓库",
+            "Github",
+            "DockerHub",
+            "Pinterest",
+            "Behance",
+            "Dribbble",
+            "Medium",
+            "Youtube"
+        ]
+    ],
+    [
+        "博客",
+        "fa-coffee",
+        [
+            "GXNAS",
+            "P3terxZone",
+            "reuixiy",
+            "三言字体"
+        ]
+    ],
+    [
+        "资源下载",
+        "fa-download",
+        [
+            "锯齿一号",
+            "果核剥壳",
+            "uu_win"
+        ]
+    ]
 ]
 
-# 博客
-TaxonomyInfo[1]["links"] = [
-    GXNAS,
-    P3terxZone,
-    reuixiy,
-    三言字体
-]
+# 有子分类的数据
+# TaxonomyInfoWithTerms_infos = []
 
-# 资源下载
-TaxonomyInfo[2]["links"] = [
-    锯齿一号,
-    果核剥壳,
-    uu_win
+# 添加没有子目录的函数
+# 参数 TaxonomyInfoWithoutTerm_website 必须以 list 方式存在
+def add_TaxonomyInfoWithoutTerm(TaxonomyInfoWithoutTerm_name,TaxonomyInfoWithoutTerm_icon,TaxonomyInfoWithoutTerm_website):
+    TaxonomyInfoWithoutTerm = {
+        "taxonomy": "无子分类主分类名",
+        "icon": "fa-star",
+        "links": []
+    }
+    TaxonomyInfoWithoutTerm["taxonomy"] = TaxonomyInfoWithoutTerm_name
+    TaxonomyInfoWithoutTerm["icon"] = TaxonomyInfoWithoutTerm_icon
+    TaxonomyInfoWithoutTerm["links"] = []
+    for i in TaxonomyInfoWithoutTerm_website:
+        if i in WebSiteInfo:
+            TaxonomyInfoWithoutTerm["links"].append(WebSiteInfo[i])
+    return TaxonomyInfoWithoutTerm
 
-]
+# 添加有子目录的函数
+# def add_TaxonomyInfoWithTerm(TaxonomyInfoWithTerm_name,TaxonomyInfoWithTerm_icon,TaxonomyInfoWithtTerm_website):
+#     TaxonomyInfoWithTerm = {
+#         "taxonomy": "有子分类网站分类",
+#         "icon": "分类标签",
+#         "list": [
+#             {
+#                 "term": "子分类名字",
+#                 "links": []
+#             }
+#         ]
+#     }
+#     TaxonomyInfoWithTerm["taxonomy"] = TaxonomyInfoWithTerm_name
+#     TaxonomyInfoWithTerm["icon"] = TaxonomyInfoWithTerm_icon
+#     return TaxonomyInfoWithTerm
 
-# 实用工具
-TaxonomyInfo[3]["links"] = [
-    IP可用性检测工具
-]
-# 设计 --> 三维设计
-TaxonomyInfo[4]["list"][0]["links"] = [
-    blender,
-    材质贴图
-]
+# 整合没有子目录的网站
+for i in TaxonomyInfoWithoutTerms_infos:
+    TaxonomyInfoWithoutTerms.append(add_TaxonomyInfoWithoutTerm(i[0],i[1],i[2]))
 
-# 设计 --> 包装设计
-TaxonomyInfo[4]["list"][1]["links"] = [
-    世界创意包装
-]
-
-# 设计 --> 工业设计
-TaxonomyInfo[4]["list"][2]["links"] = [
-    普象网,
-    二维码演示
-]
+# 整合所有的网站
+# websites = TaxonomyInfoWithoutTerms + TaxonomyInfoWithTerms
 
 
-# 开始写入，生成的文件在 /配置文件
+# 目前只支持没有子分类的生成
+# 开始写入，生成的文件在
 yaml.indent(mapping=2, sequence=4, offset=2)
 yaml.top_level_colon_align = True
 yaml.default_flow_style = False
 yaml.explicit_start = True
 with open('./yaml_update/webstack.yml', 'w', encoding='utf-8') as f:
-    yaml.dump(TaxonomyInfo, f)
+    yaml.dump(TaxonomyInfoWithoutTerms, f)
